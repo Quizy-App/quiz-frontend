e2cf32759429f4cc8b9f57ccc91dd7997207a57d
abhishek-designs <akalyane980@gmail.com>
2022-05-24 00:38:08 +0530

    Latest commit

--- src/components/screens/teachers/TeacherAddQuestions.tsx
+++ src/components/screens/teachers/TeacherAddQuestions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CustomButton from "../../CustomButton";
 import cn from "classnames";
 import { overrideTailwindClasses as ov } from "tailwind-override";
